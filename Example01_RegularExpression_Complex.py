fdf38f9364478ca41d859afcaf1205dabc1f8078
Amit Suneja <amitsuneja.devops@gmail.com>
2018-11-30 12:26:43 +0530

    Update Example01_RegularExpression_Complex.py

--- Example01_RegularExpression_Complex.py
+++ Example01_RegularExpression_Complex.py
@@ -69,3 +69,53 @@ print(re.findall(r'([^=]+)=([^=]+)(?:,|$)', string))
 # =          # equals is how we tokenise the original string
 # ([^=]+)    # value
 # (?:,|$)    # value terminator, either comma or end of string
+
+
+
+
+
+print("___________________________________________________________")
+
+
+# Look ahead positive (?=)
+# A(?=B)  ->Find expression A where expression B follows it
+
+# Look ahead negative (?!)
+# A(?!B)  ->Find expression A where expression B does not follow:
+
+# Look behind positive (?<=)
+# (?<=B)A ->Find expression A where expression B precedes:
+
+# Look behind negative (?<!)
+# (?<!B)A ->Find expression A where expression B does not precede:
+
+
+#  1. Lookbehind
+#  Question : Find all numbers which are not separated from the front word by a white space.
+
+string = "my phone number is010292291, this is a cool phone number: 69530732. Yeah1414757474 is another phone number."
+# The naive solution is to use the following regex: [A-Za-z]\d+. This will return all strings that start with a
+# letter and many digits after it, and in this case, they are s010292291 and h1414757474.
+# And then, we can iterate through them, remove the first character and get the number.
+pattern = "([A-Za-z])\d+"  # ['s', 'h']
+print(re.findall(pattern, string))
+pattern = "(?:[A-Za-z])\d+"  # ['s010292291', 'h1414757474']
+print(re.findall(pattern, string))
+
+
+# But that does not seem to be a very good solution. Why we have to manually remove the first letter?
+# Why we can not just get the number I need without the letter? That's when we need to use Lookbehind.
+# The expression (?<=[A-Za-z]) is called lookbehind expression
+# (?<=B)A ->Find expression A where expression B precedes:
+pattern = "(?<=[A-Za-z])\d+"
+print(re.findall(pattern, string))  # ['010292291', '1414757474']
+
+print("______________________________________________________")
+
+# 2. Lookahead
+# It is basically the same as lookbehind, the only difference is it matches characters at the
+# end of the string. The syntax is slightly different, we use (?!)
+# A(?!B)  ->Find expression A where expression B does not follow:
+pattern = "\d+(?![A-Za-z])"
+print(re.findall(pattern, string))  # ['010292291', '69530732', '1414757474']
+print("______________________________________________________")
